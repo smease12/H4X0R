5c30b1dc2fc542fd19214bf79085148b2936018f
smease12 <scmease93@gmail.com>
2024-09-22 20:37:55 -0400

    Add NetworkManager model

---
+++ H4X0R/Models/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  H4X0R
+//
+//  Created by Serena  on 9/22/24.
+//
+
+import Foundation
+
+class NetworkManager{
+    
+    func fetchData(){
+        
+    }
+    
+}
