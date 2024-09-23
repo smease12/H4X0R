ab1aaec969f71a07a2eafab326ec07a174467f47
smease12 <scmease93@gmail.com>
2024-09-22 21:15:45 -0400

    Update PostData model

--- H4X0R/Models/NetworkManager.swift
+++ H4X0R/Models/NetworkManager.swift
@@ -14,9 +14,17 @@ class NetworkManager{
             let task = session.dataTask(with: url){ (data, response, error) in
                 if error == nil{
                     let decoder = JSONDecoder()
+                    if let safeData = data{
+                        do{
+                          let results = try decoder.decode(Results.self, from: safeData)
+                        }
+                        catch{
+                            print(error)
+                        }
+                    }
                 }
-                
             }
+            task.resume()
         }
     }
 }
