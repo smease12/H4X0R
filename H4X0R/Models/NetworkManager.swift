e668f5b3cb00de3a1b12f428eb2b831a7dd491a1
smease12 <scmease93@gmail.com>
2024-09-22 21:01:38 -0400

    Update fetchData on NetworkManager model

--- H4X0R/Models/NetworkManager.swift
+++ H4X0R/Models/NetworkManager.swift
@@ -10,7 +10,14 @@ import Foundation
 class NetworkManager{
     
     func fetchData(){
-        
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page"){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url){ (data, response, error) in
+                if error == nil{
+                    let decoder = JSONDecoder()
+                }
+                
+            }
+        }
     }
-    
 }
