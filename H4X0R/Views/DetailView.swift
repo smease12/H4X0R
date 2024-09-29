efe83e806fa155c183af9a54715a59c4a6967e6d
smease12 <scmease93@gmail.com>
2024-09-29 19:38:26 -0400

    Add DetailView

---
+++ H4X0R/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  H4X0R
+//
+//  Created by Serena  on 9/29/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
